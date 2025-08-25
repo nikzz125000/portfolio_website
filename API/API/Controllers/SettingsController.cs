2b0f742f266628677f12bce521f9d4de13d195ce
roopesh123 <roopesh@borncode.in>
2025-08-25 12:41:49 +0530

    BackGroundColor api added

--- API/API/Controllers/SettingsController.cs
+++ API/API/Controllers/SettingsController.cs
@@ -124,5 +124,82 @@ namespace API.Controllers
             }
             return response;
         }
+
+        [Route("BackGroundColor/CreateOrModify")]
+        [HttpPost]
+        [ProducesResponseType(typeof(CommonEntityResponse), 200)]
+        [Authorize]
+        public async Task<CommonEntityResponse> CreateOrModifyBackGroundColorAsync([FromBody] BackgroundColorSetting model)
+        {
+            CommonEntityResponse response = new CommonEntityResponse();
+            try
+            {
+                response = await _settingService.CreateOrModifyBackgroundColorSetting(model);
+            }
+            catch (Exception e)
+            {
+                response.CreateFailureResponse(CommonData.ErrorMessage);
+
+                ExceptionLog log = new ExceptionLog();
+                log.Api = $@"api/Settings/BackGroundColor/CreateOrModify";
+                log.ApiType = ApiType.Post;
+                log.Parameters = "form-data";
+                log.Message = e.Message;
+                log.StackTrace = e.StackTrace;
+                await SaveExceptionLog(log);
+            }
+            return response;
+        }
+        [Route("BackGroundColor/GetAll")]
+        [HttpGet]
+        [Authorize]
+        [ProducesResponseType(typeof(ModelEntityResponse<List<BackgroundColorSetting>>), 200)]
+        public async Task<ModelEntityResponse<List<BackgroundColorSetting>>> GetAllBackGroundColor()
+        {
+            ModelEntityResponse<List<BackgroundColorSetting>> response = new ModelEntityResponse<List<BackgroundColorSetting>>();
+            try
+            {
+                //getCurrentUser();
+                response = await _settingService.GetAllBackgroundColorSetting();
+            }
+            catch (Exception e)
+            {
+                response.CreateFailureResponse(CommonData.ErrorMessage);
+
+                ExceptionLog log = new ExceptionLog();
+                log.Api = $@"api/Settings/BackGroundColor/GetAll";
+                log.ApiType = ApiType.Get;
+                log.Parameters = $@"";
+                log.Message = e.Message;
+                log.StackTrace = e.StackTrace;
+                await SaveExceptionLog(log);
+            }
+            return response;
+        }
+        [Route("BackGroundColor/Details/{Title}")]
+        [HttpGet]
+        [ProducesResponseType(typeof(ModelEntityResponse<BackgroundColorSetting>), 200)]
+        public async Task<ModelEntityResponse<BackgroundColorSetting>> GetBackgroundColorSettingByTitle(string Title)
+        {
+            ModelEntityResponse<BackgroundColorSetting> response = new ModelEntityResponse<BackgroundColorSetting>();
+            try
+            {
+                //getCurrentUser();
+                response = await _settingService.GetBackgroundColorSettingByTitle(Title);
+            }
+            catch (Exception e)
+            {
+                response.CreateFailureResponse(CommonData.ErrorMessage);
+
+                ExceptionLog log = new ExceptionLog();
+                log.Api = $@"api/Settings/BackGroundColor/Details/{Title}";
+                log.ApiType = ApiType.Get;
+                log.Parameters = $@"";
+                log.Message = e.Message;
+                log.StackTrace = e.StackTrace;
+                await SaveExceptionLog(log);
+            }
+            return response;
+        }
     }
 }
